05eaf780710c116c5e748ead8fced0a27856129d
Mian Muhammad Faraz <91678575+M-Muhammad-Faraz@users.noreply.github.com>
2023-01-22 04:21:01 +0500

    Update auth-teacher.route.js

--- routes/AuthenticateTeacher/auth-teacher.route.js
+++ routes/AuthenticateTeacher/auth-teacher.route.js
@@ -3,27 +3,40 @@ const router = express.Router();
 const { connection, auth, db } = require("../../settings/setting");
 router.post("/", (req, res) => {
   const data = req.body;
+
   console.log(data);
-  connection.query(
-    `SELECT teacher_id FROM teachers where teacher_email = '${data.email}' and teacher_password = '${data.password}'`,
-    (err, result) => {
-      if (err) {
-        res.status(400).json({
-          err: "Invalid Credentials",
-        });
+  if (data.email !== "" && data.password !== "") {
+    connection.query(
+      "SELECT * FROM teachers WHERE teacher_email = ?",
+      [data.email],
+      (err, result) => {
+        if (err) {
+          res.status(500).json({ err: "Internal Server Error" });
+        }
+        console.log(result);
+        if (result.length === 0) {
+          res.status(401).json({ err: "Email does not Exist" });
+        } else {
+          if (result[0].teacher_password !== data.password) {
+            res.status(401).json({ err: "Invalid password" });
+          } else {
+            auth
+              .getAuth()
+              .createCustomToken(result[0].teacher_id)
+              .then((customToken) => {
+                res.status(200).json({ msg: "Success", token: customToken });
+              })
+              .catch((error) => {
+                res.status(500).json({
+                  err: error.toString(),
+                });
+              });
+          }
+        }
       }
-      auth
-        .getAuth()
-        .createCustomToken(result[0].teacher_id)
-        .then((customToken) => {
-          res.status(200).json({ msg: "Success", token: customToken });
-        })
-        .catch((error) => {
-          res.status(500).json({
-            err: error,
-          });
-        });
-    }
-  );
+    );
+  } else {
+    res.status(400).json({ err: "Empty input fields" });
+  }
 });
 module.exports = router;
