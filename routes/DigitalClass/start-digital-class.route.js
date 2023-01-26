b2de58dd175ae93a68f7198846f3c40e80d2a139
Mian Muhammad Faraz <91678575+M-Muhammad-Faraz@users.noreply.github.com>
2023-01-27 02:25:22 +0500

    Added Crypt

--- routes/DigitalClass/start-digital-class.route.js
+++ routes/DigitalClass/start-digital-class.route.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const qrcode = require("qrcode");
+const crypto = require("crypto");
 const { connection } = require("../../settings/setting");
+
+function generateRandomString(length) {
+  const charset =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let result = "";
+  let bytes = crypto.randomBytes(length);
+  for (let i = 0; i < bytes.length; i++) {
+    result += charset[bytes.readUInt8(i) % charset.length];
+  }
+  return result;
+}
 
 router.post("/:lectureId", (req, res) => {
   const lecture_id = req.params.lectureId;
-  const code_id = "12345";
+  const randomString = generateRandomString(8);
+  console.log(randomString);
+  const code_id = randomString;
   const sql = `SELECT students.* FROM lecture_students
-JOIN students ON lecture_students.stud_id = students.stud_id
-WHERE lecture_id = ?`;
+  JOIN students ON lecture_students.stud_id = students.stud_id
+  WHERE lecture_id = ?`;
 
   connection.query(sql, [lecture_id], (err, results) => {
     if (err) {
@@ -16,7 +30,7 @@ WHERE lecture_id = ?`;
       return;
     }
     qrcode.toDataURL(code_id, (err, url) => {
-      res.status(200).json({ data: results, qrcode: url });
+      res.status(200).json({ data: results, qrcode: url, code: code_id });
     });
   });
 });
