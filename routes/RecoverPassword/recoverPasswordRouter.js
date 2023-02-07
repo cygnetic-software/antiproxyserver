9b05709523a5d0d62dbc4511875e68c5e564bf20
Mian Muhammad Faraz <91678575+M-Muhammad-Faraz@users.noreply.github.com>
2023-02-07 11:35:29 +0500

    xyz

--- routes/RecoverPassword/recoverPasswordRouter.js
+++ routes/RecoverPassword/recoverPasswordRouter.js
@@ -3,13 +3,7 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-const {
-  connection,
-  auth,
-  db,
-  transporter,
-  actionCodeSettings,
-} = require("../../settings/setting");
+const { connection, auth } = require("../../settings/setting");
 
 router.post("/student", (req, res) => {
   console.log("recovering student");
@@ -23,37 +17,27 @@ router.post("/student", (req, res) => {
       }
 
       if (result.length !== 0) {
-        auth
-          .getAuth()
-          .generatePasswordResetLink(email, actionCodeSettings)
-          .then((link) => {
-            // Construct password reset email template, embed the link and send
-            const data = {
-              service_id: "service_m1v4l14",
-              template_id: "template_dmfukbs",
-              user_id: "xp-KMU60lbVqCzHoH",
-              template_params: {
-                email: email,
-                link: link,
-                name: result[0].name,
-              },
-              accessToken: "59qXHnKaCRrNH_D8FV7xU",
-            };
-            axios
-              .post("https://api.emailjs.com/api/v1.0/email/send", data)
-              .then((e) => {
-                console.log("Email Sent");
-                res.status(200).json({ message: "Success" });
-              })
-              .catch((e) => {
-                console.log(e);
-                res.status(400).json({ message: "Error" });
-              });
+        // Construct password reset email template, embed the link and send
+        const data = {
+          service_id: "service_m1v4l14",
+          template_id: "template_dmfukbs",
+          user_id: "xp-KMU60lbVqCzHoH",
+          template_params: {
+            email: email,
+            link: `http://localhost:3000/reset-password?email=${email}`,
+            name: result[0].name,
+          },
+          accessToken: "59qXHnKaCRrNH_D8FV7xU",
+        };
+        axios
+          .post("https://api.emailjs.com/api/v1.0/email/send", data)
+          .then((e) => {
+            console.log("Email Sent");
+            res.status(200).json({ message: "Success" });
           })
-          .catch((error) => {
-            res.status(500).json({ message: "System Faliure", error: error });
-
-            // Some error occurred.
+          .catch((e) => {
+            console.log(e);
+            res.status(400).json({ message: "Error" });
           });
       }
     }
@@ -63,7 +47,7 @@ router.post("/teacher", (req, res) => {
   console.log("recovering teacher");
   const { email } = req.body;
   connection.query(
-    "SELECT * FROM teachers WHERE teacher_email = ?",
+    "SELECT * FROM teacher WHERE email = ?",
     [email],
     (err, result) => {
       if (err) {
@@ -71,37 +55,27 @@ router.post("/teacher", (req, res) => {
       }
 
       if (result.length !== 0) {
-        auth
-          .getAuth()
-          .generatePasswordResetLink(email, actionCodeSettings)
-          .then((link) => {
-            // Construct password reset email template, embed the link and send
-            const data = {
-              service_id: "service_m1v4l14",
-              template_id: "template_dmfukbs",
-              user_id: "xp-KMU60lbVqCzHoH",
-              template_params: {
-                email: email,
-                link: link,
-                name: result[0].teacher_name,
-              },
-              accessToken: "59qXHnKaCRrNH_D8FV7xU",
-            };
-            axios
-              .post("https://api.emailjs.com/api/v1.0/email/send", data)
-              .then((e) => {
-                console.log("Email Sent");
-                res.status(200).json({ message: "Success" });
-              })
-              .catch((e) => {
-                console.log(e);
-                res.status(400).json({ message: "Error" });
-              });
+        // Construct password reset email template, embed the link and send
+        const data = {
+          service_id: "service_m1v4l14",
+          template_id: "template_dmfukbs",
+          user_id: "xp-KMU60lbVqCzHoH",
+          template_params: {
+            email: email,
+            link: `http://localhost:3000/reset-password-teacher?email=${email}`,
+            name: result[0].name,
+          },
+          accessToken: "59qXHnKaCRrNH_D8FV7xU",
+        };
+        axios
+          .post("https://api.emailjs.com/api/v1.0/email/send", data)
+          .then((e) => {
+            console.log("Email Sent");
+            res.status(200).json({ message: "Success" });
           })
-          .catch((error) => {
-            res.status(500).json({ message: "System Faliure", error: error });
-
-            // Some error occurred.
+          .catch((e) => {
+            console.log(e);
+            res.status(400).json({ message: "Error" });
           });
       }
     }
--- routes/UpdatePassword/update-password.route.js
+++ routes/UpdatePassword/update-password.route.js
@@ -6,33 +6,68 @@ const { connection, auth, db } = require("../../settings/setting");
 
 router.post("/student", (req, res) => {
   const { email, password } = req.body;
-  connection.query(
-    "UPDATE students SET password = ? WHERE email = ?",
-    [password, email],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        res.status(500).json({ message: err });
-      } else {
-        res.status(200).json({ message: "Password Updated" });
-      }
+  console.log(email, password);
+  const sql = "SELECT * FROM students WHERE email = ?";
+  connection.query(sql, [email], (err, users) => {
+    if (err) {
+      console.log(err);
+      throw err;
     }
-  );
+    if (users.length !== 0) {
+      auth
+        .getAuth()
+        .updateUser(users[0].stud_id, { password: password })
+        .then(() => {
+          connection.query(
+            "UPDATE students SET password = ? WHERE email = ?",
+            [password, email],
+            (err, result) => {
+              if (err) {
+                console.log(err);
+                res.status(500).json({ message: err });
+              } else {
+                console.log("done");
+                res.status(200).json({ message: "Password Updated" });
+              }
+            }
+          );
+        })
+        .catch((e) => {
+          res.status(500).json({ message: e });
+        });
+    } else {
+      res.status(400).json({ message: "User not found" });
+    }
+  });
 });
 router.post("/teacher", (req, res) => {
   const { email, password } = req.body;
-  connection.query(
-    "UPDATE teachers SET teacher_password = ? WHERE teacher_email = ?",
-    [password, email],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        res.status(500).json({ message: err });
-      } else {
-        res.status(200).json({ message: "Password Updated" });
-      }
+  const sql = "SELECT * FROM teachers WHERE teacher_email = ?";
+  connection.query(sqt, [email], (err, users) => {
+    if (err) {
+      console.log(err);
+      throw err;
     }
-  );
+    if (users.length !== 0) {
+      auth
+        .getAuth()
+        .updateUser(users[0].teacher_id, { password: password })
+        .then(() => {
+          connection.query(
+            "UPDATE teachers SET teacher_password = ? WHERE teacher_email = ?",
+            [password, email],
+            (err, result) => {
+              if (err) {
+                console.log(err);
+                res.status(500).json({ message: err });
+              } else {
+                res.status(200).json({ message: "Password Updated" });
+              }
+            }
+          );
+        });
+    }
+  });
 });
 
 module.exports = router;
